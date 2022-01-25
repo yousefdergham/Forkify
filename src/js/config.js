3b8c0bcf0b1a52afc3fdeb98b809283ed38ea176
Yousef Dergham <79238462+yousefdergham@users.noreply.github.com>
2022-01-25 20:32:34 +0200

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = '74c55250-af49-491e-818f-b8c08f6a568c';
 export const MODAL_CLOSE_SEC = 2.5;
